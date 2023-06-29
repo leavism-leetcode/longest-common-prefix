a7ac2da8fdf5b022dc7deb20049e5fd8ed9ce86a
Huy Dang <ghuydang@gmail.com>
2023-06-29 09:30:39 -0700

    Covered edge cases. Build passes but slow

--- src/Solution.java
+++ src/Solution.java
@@ -3,28 +3,31 @@ public class Solution {
         String first = strs[0];
         String result = "";
 
+        // Edge case when it's just one word
+        if(strs.length == 1) { return strs[0]; }
+
         for(int letterIndex = 0; letterIndex < first.length(); letterIndex++) {
-            Boolean matches = false;
+            boolean matches = false;
             for(int wordIndex = 1; wordIndex < strs.length; wordIndex++) {
+                // Catches when the word we're looking at is shorter than the first word
+                if(strs[wordIndex].length() == letterIndex) { return result; }
+
                 Character goal = first.charAt(letterIndex);
                 Character lookingAt = strs[wordIndex].charAt(letterIndex);
 
                 if(lookingAt.equals(goal)) {
                     matches = true;
                 } else {
-                    matches = false;
                     return result;
                 }
             }
-
-            if(matches) { result += first.charAt((letterIndex)); }
+            if(matches) { result += first.charAt(letterIndex); }
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        String[] words = {"flower","flow","flight"};
+        String[] words = {"ab","a"};
 
         System.out.println(longestCommonPrefix(words));
     }
